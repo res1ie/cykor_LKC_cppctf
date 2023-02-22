ca29e572be6c16f477d61703792ac149cdee2102
yi-jongwoo <82140757+yi-jongwoo@users.noreply.github.com>
2023-02-22 15:31:02 +0900

    미완성

--- EC.cpp
+++ EC.cpp
@@ -9,18 +9,45 @@ public:
 	field x;
 	field y;
 	EC():is_O(true),x(),y(){}
-	EC(field xx,field yy):is_O(false),x(xx),y(yy){}
+	EC(field xx,field yy):is_O(false),x(xx),y(yy){} // it must tested that is element of curve when emplementing cryptosystem, but we wont test it since it is for CTF
 	bool isLegal(){
 		return is_O || y*y == x*x*x + a*x + b;
 	}
-	EC(const EC& o){
-		return is_o
+	EC(const EC& o):is_O(o.is_O),x(o.x),y(o.y){}
+	EC operator+(const EC&o) const{
+		if(is_O)
+			return o;
+		if(o.is_O)
+			return *this;
+		if(x==o.x){
+			if(y==o.x){
+				//...
+			}
+			else
+				return EC();
+		}
+		field m = (o.y-y) / (o.x-x);
+		field newx = m*m - x - o.x;
+		field newy = y + (newx-x)*m;
+		return EC(newx,-newy); 
+	}
+	EC operator-() const{
+		if(is_O)
+			return EC();
+		return EC(x,-y);
 	}
 };
+
+template<class field,int code_num>
+field EC<field,code_num>::a;
+template<class field,int code_num>
+field EC<field,code_num>::b;
 
 int main(){
 	typedef MF<int,7> mf;
 	typedef EC<mf,1> ec;
 	ec::a=0;
-	ec::b=7; 
+	ec::b=7;
+	ec x,y;
+	x=x+y;
 }
